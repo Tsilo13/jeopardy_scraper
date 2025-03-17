19bc0c8967a377950eba7c753956cb967132e9c5
alexfishy12 <58009009+alexfishy12@users.noreply.github.com>
2025-03-16 22:09:53 -0400

    updated to get all episodes of every season

--- notebook_pasted.py
+++ notebook_pasted.py
@@ -1,4 +1,6 @@
 import requests
+import time
+import json
 
 from bs4 import BeautifulSoup
 
@@ -13,84 +15,67 @@ seasons_json_array = []
 seasons_trs = soup.find_all("tr")
 for season_tr in seasons_trs:
     # init new episode json object
-    season_json = {"id": None, "name": None, "air_date": None, "URL": None}
+    season_json = {"name": None, "from_date": None, "to_date": None, "URL": None, "episodes": []}
 
-    print(season_tr)
+    #print(season_tr)
 
     # get 'a' tag for ID, air date, and URL
     a_tag = season_tr.find("a")
-    print(a_tag)
+    #print(a_tag)
     #season_json["id"] = a_tag.string.split(",")[0].lstrip("#")
     #season_json["air_date"] = a_tag.string[-10:]
-    #season_json["URL"] = base_url + a_tag["href"]
+    season_json["URL"] = base_url + a_tag["href"]
+    season_json["name"] = a_tag.string
 
     # get 'td' tag for episode name
     td = season_tr.find_all('td')[1]
     string = td.string.lstrip().rstrip()
-    season_json["name"] = string
+    if string.find(" to ") > 0:
+        dates = string.split(" to ")
+        #print(dates)
+        season_json["from_date"] = dates[0]
+        season_json["to_date"] = dates[1]
 
     # append episode json to array
     seasons_json_array.append(season_json)
 
-print(seasons_json_array)
-
-
-
+#print(seasons_json_array)
 
 
 # GET A SEASON'S EPISODES
-season_episodes_URL = base_url + "showseason.php?season=33"
-season_episodes_page = requests.get(season_episodes_URL)
-soup = BeautifulSoup(season_episodes_page.content, "html.parser")
 
-episodes_json_array = []
-episodes = soup.find_all("tr")
-for episode in episodes:
-    # init new episode json object
-    episode_json = {"id": None, "name": None, "air_date": None, "URL": None}
+for season in seasons_json_array:
 
-    # get 'a' tag for ID, air date, and URL
-    a_tag = episode.find("a")
-    episode_json["id"] = a_tag.string.split(",")[0].lstrip("#")
-    episode_json["air_date"] = a_tag.string[-10:]
-    episode_json["URL"] = base_url + a_tag["href"]
+    season_episodes_URL = season["URL"]
+    season_episodes_page = requests.get(season_episodes_URL)
+    soup = BeautifulSoup(season_episodes_page.content, "html.parser")
 
-    # get 'td' tag for episode name
-    td = episode.find_all('td')[1]
-    string = td.string.lstrip().rstrip()
-    episode_json["name"] = string
+    episodes_json_array = []
+    episodes = soup.find_all("tr")
+    for episode in episodes:
+        # init new episode json object
+        episode_json = {"id": None, "name": None, "air_date": None, "URL": None}
 
-    # append episode json to array
-    episodes_json_array.append(episode_json)
+        # get 'a' tag for ID, air date, and URL
+        a_tag = episode.find("a")
+        if a_tag.string is not None:
+            episode_json["id"] = a_tag.string.split(",")[0].lstrip("#")
+            episode_json["air_date"] = a_tag.string[-10:]
+        episode_json["URL"] = base_url + a_tag["href"]
 
-print(episodes_json_array)
+        # get 'td' tag for episode name
+        td = episode.find_all('td')[1]
+        string = td.string.lstrip().rstrip()
+        episode_json["name"] = string
+
+        # append episode json to array
+        episodes_json_array.append(episode_json)
+    
+    # wait one second as to not get blocked from the website
+    time.sleep(0.5)
+
+    season["episodes"] = episodes_json_array
 
 
-
-
-# GET AN EPISODE'S CLUES / ANSWERS
-season_33 = base_url + "showseason.php?season=33"
-page = requests.get(season_33)
-soup = BeautifulSoup(page.content, "html.parser")
-
-episodes_json_array = []
-episodes = soup.find_all("tr")
-for episode in episodes:
-    # init new episode json object
-    episode_json = {"id": None, "name": None, "air_date": None, "URL": None, "clues": []}
-
-    # get 'a' tag for ID, air date, and URL
-    a_tag = episode.find("a")
-    episode_json["id"] = a_tag.string.split(",")[0].lstrip("#")
-    episode_json["air_date"] = a_tag.string[-10:]
-    episode_json["URL"] = base_url + a_tag["href"]
-
-    # get 'td' tag for episode name
-    td = episode.find_all('td')[1]
-    string = td.string.lstrip().rstrip()
-    episode_json["name"] = string
-
-    # append episode json to array
-    episodes_json_array.append(episode_json)
-
-print(episodes_json_array)
+with open("data/seasons.json", "w") as file:
+    json.dump(seasons_json_array, file, indent=4)
