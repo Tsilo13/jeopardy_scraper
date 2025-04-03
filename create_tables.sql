8e9301a9e4f23a70f9653c66cbf26b1a6140bd70
Tsilo13 <gstoik59@gmail.com>
2025-04-03 18:42:18 -0400

    added .sql and updated requirements for an insert_data.py that will load the csvs into an oracle DB

---
+++ create_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE categories (
+  category_id VARCHAR2(32) PRIMARY KEY,
+  game_id VARCHAR2(20),
+  round_name VARCHAR2(30),
+  category_name VARCHAR2(100),
+  FOREIGN KEY (game_id) REFERENCES games(game_id)
+);
+
+CREATE TABLE categories (
+  category_id VARCHAR2(32) PRIMARY KEY,
+  game_id VARCHAR2(20),
+  round_name VARCHAR2(30),
+  category_name VARCHAR2(100),
+  FOREIGN KEY (game_id) REFERENCES games(game_id)
+);
+
+CREATE TABLE clues (
+  clue_id VARCHAR2(32) PRIMARY KEY,
+  category_id VARCHAR2(32),
+  game_id VARCHAR2(20),
+  value NUMBER,
+  clue_text CLOB,
+  correct_response CLOB,
+  FOREIGN KEY (category_id) REFERENCES categories(category_id),
+  FOREIGN KEY (game_id) REFERENCES games(game_id)
+);
